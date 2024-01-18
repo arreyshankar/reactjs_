33fea6f73dfb3d5e368c2d8b60e247e015a48c70
ArreyShankar <sarveshkumar10101@gmail.com>
2024-01-18 18:46:40 +0530

    First

---
+++ script.js
@@ -0,0 +1,5 @@
+const root = document.getElementById('root');
+const paragraph = document.createElement('p');
+const paragraphContent = document.createTextNode("I'm a paragraph!");
+paragraph.appendChild(paragraphContent);
+root.appendChild(paragraph);
